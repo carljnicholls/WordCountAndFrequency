37b836e3b605b905da7eb0f4c559ef2e40293f69
Carl Nicholls <carljnicholls@hotmail.com>
2017-08-22 18:12:55 +0100

    All Unit Tests Pass

---
+++ WordCountAndFrequency/Phrase.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordCountAndFrequency
+{
+    public class Phrase
+    {
+        public static Dictionary<string, int> WordCount(string phrase)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            char[] delimiters = new char[] { ' ', ',' };
+            string[] split = phrase.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            split = RemoveSpecialChars(split);
+            dictionary = CountOccuranceOfWord(dictionary, split); 
+
+            return dictionary; 
+        }
+
+        private static string[] SanitiseStringInputs(string[] words)
+        {
+
+            return words; 
+        }
+
+        private static string[] RemoveSpecialChars(string[] wordsToCount)
+        {
+            var stack = new Stack<string>();
+
+            for (int i = 0; i < wordsToCount.Length; i++)
+            {
+                var word = "";
+
+                for (int j = 0; j < wordsToCount[i].Length; j++)
+                {
+                    if (Char.IsLetterOrDigit(wordsToCount[i][j]) || ApostropheHandling(wordsToCount[i], j))
+                        word += wordsToCount[i][j];
+                }
+
+                if(word.Length != 0)
+                    stack.Push(word);
+            }
+
+            var strArr = new string[stack.Count];
+            stack.CopyTo(strArr, 0);
+
+            return strArr;
+        }
+
+        private static bool ApostropheHandling(string word, int iterator)
+        {
+            bool returnVal = false;
+
+            if(word[iterator] == '\'')
+                if(iterator != 0 && (iterator + 1) < word.Length)
+                    if (Char.IsLetter(word[iterator - 1]) && Char.IsLetter(word[iterator + 1]))
+                        returnVal = true;                     
+
+            return returnVal;
+        }
+
+        private static Dictionary<string, int> CountOccuranceOfWord(Dictionary<string, int> dictionary, string[] wordsToCount)
+        {
+            for (int i = 0; i < wordsToCount.Length; i++)
+            {
+                string word = "";
+
+                word = wordsToCount[i].ToLower();
+
+                if (dictionary.ContainsKey(word))
+                {
+                    int val = dictionary[word];
+                    val++;
+                    dictionary[word] = val;
+                }
+                else
+                    dictionary.Add(word, 1);
+            }
+            return dictionary; 
+        }
+    }
+}
---
+++ WordCountAndFrequencyTests/WordCountTests.cs
@@ -0,0 +1,159 @@
+using System.Collections.Generic;
+using WordCountAndFrequency;
+using Xunit;
+
+namespace WordCountAndFrequencyTests
+{
+    public class WordCountTests
+    {
+        [Fact]
+        public void Count_one_word()
+        {
+            var counts = new Dictionary<string, int>
+            {
+                { "word", 1 }
+            };
+
+            Assert.Equal(counts, Phrase.WordCount("word"));
+        }
+
+        [Fact]
+        public void Count_one_of_each()
+        {
+            var counts = new Dictionary<string, int>
+            {
+                { "one",  1 },
+                { "of",   1 },
+                { "each", 1 }
+            };
+
+            Assert.Equal(counts, Phrase.WordCount("one of each"));
+        }
+
+        [Fact]
+        public void Count_multiple_occurrences()
+        {
+            var counts = new Dictionary<string, int>
+            {
+                { "one",  1 },
+                { "fish", 4 },
+                { "two",  1 },
+                { "red",  1 },
+                { "blue", 1 },
+            };
+
+            Assert.Equal(counts, Phrase.WordCount("one fish two fish red fish blue fish"));
+        }
+
+        [Fact]
+        public void Count_everything_just_once()
+        {
+            var counts = new Dictionary<string, int>
+            {
+                { "all",    2 },
+                { "the",    2 },
+                { "kings",  2 },
+                { "horses", 1 },
+                { "and",    1 },
+                { "men",    1 },
+            };
+
+            Assert.Equal(counts, Phrase.WordCount("all the kings horses and all the kings men"));
+        }
+
+        [Fact]
+        public void Ignore_punctuation()
+        {
+            var counts = new Dictionary<string, int>
+            {
+                { "car",        1 },
+                { "carpet",     1 },
+                { "as",         1 },
+                { "java",       1 },
+                { "javascript", 1 },
+            };
+
+            Assert.Equal(counts, Phrase.WordCount("car : carpet as java : javascript!!&@$%^&"));
+        }
+
+        [Fact]
+        public void Handles_cramped_list()
+        {
+            var counts = new Dictionary<string, int>
+            {  
+                { "one",   1 },
+                { "two",   1 },
+                { "three", 1 },
+            };
+
+            Assert.Equal(counts, Phrase.WordCount("one,two,three"));
+        }
+
+        [Fact]
+        public void Include_numbers()
+        {
+            var counts = new Dictionary<string, int>
+            {
+                { "testing", 2 },
+                { "1",       1 },
+                { "2",       1 },
+            };
+
+            Assert.Equal(counts, Phrase.WordCount("testing, 1, 2 testing"));
+        }
+
+        [Fact]
+        public void Normalize_case()
+        {
+            var counts = new Dictionary<string, int>
+            {
+                { "go", 3 },
+            };
+
+            Assert.Equal(counts, Phrase.WordCount("go Go GO"));
+        }
+
+        [Fact]
+        public void With_apostrophes()
+        {
+            var counts = new Dictionary<string, int>
+            {
+                { "first", 1 },
+                { "don't", 2 },
+                { "laugh", 1 },
+                { "then",  1 },
+                { "cry",   1 },
+            };
+
+            Assert.Equal(counts, Phrase.WordCount("First: don't laugh. Then: don't cry."));
+        }
+
+        [Fact]
+        public void With_free_standing_apostrophes()
+        {
+            var counts = new Dictionary<string, int>
+            {
+                { "go", 3 },
+            };
+
+            Assert.Equal(counts, Phrase.WordCount("go ' Go '' GO"));
+        }
+
+        [Fact]
+        public void With_apostrophes_as_quotes()
+        {
+            var counts = new Dictionary<string, int>
+            {
+                { "she", 1 },
+                { "said", 1 },
+                { "let's", 1 },
+                { "meet", 1 },
+                { "at", 1 },
+                { "twelve", 1 },
+                { "o'clock", 1 },
+            };
+
+            Assert.Equal(counts, Phrase.WordCount("She said, 'let's meet at twelve o'clock'"));
+        }
+    }
+}
